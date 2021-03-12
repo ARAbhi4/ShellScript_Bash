f58548578d019641df2a848959ada5ec12e389ea
Muhammad Anisur Rahman <41234183+ARAbhi4@users.noreply.github.com>
2021-03-12 19:35:34 +0600

    Add files via upload

---
+++ 2nd_highest_and_3rd_highest.sh
@@ -0,0 +1,30 @@
+echo "Enter your array size: "
+read n
+echo "Enter array elements: "
+
+i=0
+while ((i < $n))
+do 
+    read -p "Enter the number: " arr[$i]
+    ((i++))
+done
+   
+for ((i = 0; i<5; i++)) 
+do
+      
+    for((j = 0; j<5-i-1; j++)) 
+    do
+      
+        if [ ${arr[j]} -lt ${arr[$((j+1))]} ] 
+        then
+            temp=${arr[j]} 
+            arr[$j]=${arr[$((j+1))]}   
+            arr[$((j+1))]=$temp 
+        fi
+    done
+done
+
+
+sum=$((${arr[1]} + ${arr[2]}))
+
+echo "The sum of 2nd and 3rd element is: ${arr[1]} + ${arr[2]} = $sum "
